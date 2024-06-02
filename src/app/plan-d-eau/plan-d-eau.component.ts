4a9f5a3e5794c08b8529a70197c1f22551d5854e
AmalCh2 <testtestgogo55@gmail.com>
2024-06-02 04:08:15 +0200

    VERSION 6
    affichage plan d'eau

--- src/app/plan-d-eau/plan-d-eau.component.ts
+++ src/app/plan-d-eau/plan-d-eau.component.ts
@@ -10,6 +10,12 @@ import { TypeBateauService } from 'src/app/shared/Service/TypeBateau.service';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import * as pdfMake from 'pdfmake/build/pdfMake'
 import * as pdfFonts from 'pdfmake/build/vfs_fonts'
+import { QuaiService } from 'src/app/shared/Service/Quai.service';
+import { PElectriqueService } from 'src/app/shared/Service/PElectrique.service';
+import { Quai } from 'src/app/shared/Model/Quai';
+import { PElectrique } from 'src/app/shared/Model/PElectrique';
+import { Emplacement } from 'src/app/shared/Model/Emplacement';
+import { EmplacementService } from 'src/app/shared/Service/Emplacement.service';
 
 @Component({
   selector: 'app-plan-d-eau',
@@ -30,14 +36,14 @@ export class PlanDEauComponent implements OnInit {
     return styleClass;
   }
 
-  listBateau: any;
-  bateau!: Bateau;
+  listQuai: any;
+  quai!: Quai;
 
-  listClient: any;
-  Client!: Client;
+  listPElectrique: any;
+  pElectrique!: PElectrique;
 
-  listTypeBateau: any;
-  TypeBateau!: TypeBateau;
+  emplacement!: Emplacement;
+  listEmplacement: any;
 
 
 
@@ -46,14 +52,8 @@ export class PlanDEauComponent implements OnInit {
   screenWidth = 0;
   data: string[] = [];
 
-  constructor(private axiosService: AxiosService,
-    private BateauService: BateauService , 
-    private typeBateauService: TypeBateauService , 
-    private ClientService: ClientService
-    , private modalService: NgbModal 
-
-
-  ) {}
+  constructor(private axiosService: AxiosService, private emplacementService: EmplacementService, private modalService: NgbModal , private quaiService: QuaiService, private p_electriqueService: PElectriqueService) {
+  }
 
   ngOnInit(): void {
     this.axiosService.request(
@@ -62,113 +62,52 @@ export class PlanDEauComponent implements OnInit {
       null
     ).then(
       (response) => this.data = response.data
+
     );
-    this.getAllClients();
-    this.Client = {
-      id_cli: null,
-    nom_cli: null,
-    prenom_cli: null,
-    etat_civil: null,
-    adresse_cli: null,
-    ville_cli: null,
-    tel_cli: null,
-    fax_cli: null,
-    mobile_cli: null,
-    email_cli: null,
-    exo_cli: null,
-    pays:null,
-    code_postal_cliii:null
-    
-    };
-
-    this.getAllTypeBateaus();
-
-    this.TypeBateau = {
-        id_type_bat: null,
-        type_bat:null,
-        designation_bat:null,
-        majoration_bat:null,
-        multicoque:null,
-    }
-
-    this.getAllBateaux();
-
-    this.bateau = {
-      id_bat:null,
-     immatriculation_bat:null,
-     autre_ident_nom_bat:null,
-     nom_bat:null,
-     largeur_bat:null,
-     longueur_bat:null,
-     tirant_eau_bat:null,
-     tonnage_bat:null,
-     pavillon_bat:null,
-     marque_bat:null,
-     num_assur:null,
-     nom_assur:null,
-     date_exp:null,
-     adresse_bat:null,
-     code_postal_bat:null,
-     ville_bat:null,
-     pays:null,
-     tel_bat:null,
-     fax_bat:null,
-     mobile_bat:null,
-     email_bat:null,
-     observation:null,
-     typeBateau:null,
-     client:null,
-     port:null, 
-     
-     date_mvt:null,
-     depart_mvt:null,
-     arrivee_mvt:null,
-     commentaire_mvt:null,
-     id_emp:null,
-    }
-
-  }
-
-  getAllTypeBateaus() {
-    this.typeBateauService.getAllTypeBateau().subscribe((data: TypeBateau[]) => {
-      this.listTypeBateau = data;
-    });
-  }
-
-  getAllClients() {
-    this.ClientService.getAllClients().subscribe(res => this.listClient = res)
-  }
-
-
-
-
-  getAllBateaux() {
-    this.BateauService.getAllBateaux().subscribe(res => this.listBateau = res)
-  }
-
-  printBateau(bateau: any) {
-    let docDefinition: any = {
-      content: [
-        { text: 'My Client', style: 'title' }, // Title element
-        { text: '\n' }, // Add some space after the title
-        { text: 'Bateau ID: ', bold: true },
-        { text: bateau.id_bat + '\n', bold: true, color: 'blue' },
-        { text: 'bateau Name: ', bold: true },
-        { text: bateau.nom_bat + '\n', bold: true, italic: true },
-      ],
-      styles: {
-        title: { // Define a custom style for the title
-          bold: true,
-          fontSize: 20, // Larger font size
-          alignment: 'center', // Center alignment
-          color: 'orange', // Orange color
-          margin: [0, 0, 0, 20] // Margin bottom to add space below the title
-        }
+    this.getAllEmplacements();
+    this.emplacement = {
+      id_emp: null,
+      largeur_emp: null,
+      longueur_emp: null,
+      tirant_eau_emp: null,
+      nbr_mouillage_emp: null,
+      type_cord_emp: null,
+      commentaire: null,
+      rang: null,
+      blockage: null,
+      direction_emp: null,
+      pelectrique: null,
+      quai: null,
+  
       }
-    };
+      this.getAllQuais();
+      this.quai = {
+        id_quai: null,
+        desig_quai: null,
+        rang_quai: null,
+        }
+  
+        this.getAllPElectriques();
+  
+      this.pElectrique = {
+        id_p_elec: null,
+        designation: null,
+      };
 
-    pdfMake.createPdf(docDefinition).open();
   }
+
+  getAllPElectriques() {
+    this.p_electriqueService.getAllPElectriques().subscribe(res => this.listPElectrique = res)
+  }
+  
+  getAllQuais() {
+    this.quaiService.getAllQuais().subscribe(res => this.listQuai = res)
+  }
+  getAllEmplacements() {
+    this.emplacementService.getAllEmplacements().subscribe(res => this.listEmplacement = res)
+  }
+
+  
 
   onToggleSideNav(data: SideNavToggle): void {
     this.screenWidth = data.screenWidth;
